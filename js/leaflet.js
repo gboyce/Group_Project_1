6312493048d9a22aee5c01ff9f81ed0824d88662
Danny Suazo <dannysuazo@MacBook-Pro.local>
2017-04-21 03:25:24 -0500

    added more styles

--- js/leaflet.js
+++ js/leaflet.js
@@ -7,7 +7,7 @@ function initialize() {
   var mapMinZoom = 0;
   var mapMaxZoom = 5;
   var center = [0, 0];
-  var options = {zoom: 2.8, position: center};
+  var options = {zoom: 2.4, position: center};
   earth = new WE.map('mapid', options);
   //Calls the map image layer from the Mapbox url -
   var layer = WE.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZHVieWEiLCJhIjoiY2owZDFoMHRsMDAydDMzb2QzZWUycDNiayJ9.VWmWF8th7jYz_fTrJVwUpA', {
@@ -38,12 +38,12 @@ console.log("Latitude: " +e.latitude + ', ' + " Longitude: " + e.longitude);
 }
 earth.on('click', showInfo);
 
-//hide start button
+//hide main start button
 $('.start-button').click(function() {
     $(this).hide();
 });
 
-//trivia window
+//main trivia window panel
 $('.start-game').click(function(){
   $('#slideout').toggleClass('on');
 });

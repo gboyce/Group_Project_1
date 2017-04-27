862e813332f0d565a6dba205570cd3fab85fe369
Greg Boyce <Greg Boyce>
2017-04-26 20:13:19 -0500

    https

--- js/spotify.js
+++ js/spotify.js
@@ -3,15 +3,15 @@ var spotifyApi = new SpotifyWebApi();
 
 // Set the variables
 
-// var trackId ='';
-// var genre ='';
+var trackId ='';
+var genre ='';
 var artist ='';
 var artist_id ='';
 var album_art ='';
 var preview_url ='';
 
 // Array of trackIds
-var trackIdArray = ['7KXjTSCq5nL1LoYtL7XAwS', '6HZILIRieu8S0iqY8kIKhj', '1EaKU4dMbesXXd3BrLCtYG'];
+var trackIdArray = ['6lEIjrQNwJPecJ7mMXjhjo', '6pjtY01rXw5UMuZvqyFIa5', '2ySKRXKN58cbIlY1cxd80y'];
 
 
 // Run on start of game
@@ -46,11 +46,10 @@ function spotifyUpdate() {
           //console.log('Mp3 Preview: ' + preview_url);
           // Insert mp3 URL into <audio>
           $('#audio-player').attr('src', preview_url);
-
       });
+
     $('#album').html(albumCover);
     // The following needs to run asynchronous
-
       spotifyApi.getArtist(artist_id, {limit: 1}, function(err, data) {
         if
           (err) console.error(err);

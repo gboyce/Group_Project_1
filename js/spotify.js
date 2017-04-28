7c04280cc0e6d7322856eb2c97a5213eb83c797c
Danny Suazo <dannysuazo@MacBook-Pro.local>
2017-04-28 03:26:19 -0500

    latest vesion

--- js/spotify.js
+++ js/spotify.js
@@ -3,13 +3,12 @@ var spotifyApi = new SpotifyWebApi();
 
 // Set the variables
 
-// var trackId ='';
-// var genre ='';
+var trackId ='';
+var genre ='';
 var artist ='';
 var artist_id ='';
 var album_art ='';
 var preview_url ='';
-
 // Array of trackIds
 var trackIdArray = ['7KXjTSCq5nL1LoYtL7XAwS', '6HZILIRieu8S0iqY8kIKhj', '1EaKU4dMbesXXd3BrLCtYG'];
 
@@ -28,6 +27,7 @@ function spotifyUpdate() {
     // If error returned
         if
           (err) console.error(err);
+
     // If return is successful
         else
 
@@ -41,14 +41,19 @@ function spotifyUpdate() {
           //console.log('Artist ID: ' + artist_id);
           album_art = data.album.images[0].url;
           albumCover.attr("src", data.album.images[0].url);
+          $('#album').html(albumCover);
           //console.log('Album Art: ' + album_art);
           preview_url = data.preview_url;
           //console.log('Mp3 Preview: ' + preview_url);
           // Insert mp3 URL into <audio>
           $('#audio-player').attr('src', preview_url);
 
+
+          counter++
+          $('#count').text('Rounds: ' + counter + ' of 8');
+
       });
-    $('#album').html(albumCover);
+
     // The following needs to run asynchronous
 
       spotifyApi.getArtist(artist_id, {limit: 1}, function(err, data) {
@@ -58,5 +63,13 @@ function spotifyUpdate() {
           genre = data.genres;
           console.log('Genre: ' + genre);
           $('#genre').text('Genre: ' + genre);
+
+
+
+
       });
+
+
+
+
 }
